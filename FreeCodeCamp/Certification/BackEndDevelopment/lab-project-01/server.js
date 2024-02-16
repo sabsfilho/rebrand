622f13a431f03a8bdd2c45a6a0d275a356e9f5d4
Samuel Santos <sabsfilho@gmail.com>
2024-02-15 22:43:19 -0300

    remove lab project

--- FreeCodeCamp/Certification/BackEndDevelopment/lab-project-01/server.js
+++
@@ -1,9 +0,0 @@
-var express = require("express");
-var app = express();
-var cors = require("cors");
-app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
-const headerparserApp = require('./headerparser/index');
-
-var listener = app.listen(process.env.PORT || 3000, function () {
-  console.log("Your app is listening on port " + listener.address().port);
-});
